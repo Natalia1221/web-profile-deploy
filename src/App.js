ef69711dbf495bb109c5aeb5043b159d64e679e3
Natalia Matondang <nataliamtd.21@gmail.com>
2023-06-06 20:30:53 +0700

    menambahkan navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import './assets/App.css';
 
+import NavbarWeb from './component/Navbar'
 import VisiMisi from './routes/Profile';
 import Kegiatan from './routes/Kegiatan';
 import Berita from './routes/Berita';
@@ -13,12 +15,13 @@ import Pengurus from './routes/Pengurus';
 function App() {
   return (
     <>
+      <NavbarWeb/>
       <main>
         <Routes>
-          <Route path="/" element={<VisiMisi />}/>
+          <Route path="/" element={<VisiMisi />} />
           <Route path="/pengurus" element={<Pengurus />} />
           <Route path="/Kegiatan" element={<Kegiatan />} />
-          <Route path="/Berita"  >
+          <Route path="/Berita">
             <Route index element={<Berita />} />
             <Route path=":id" element={<Detail />} />
           </Route>
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,35 @@
+import '../assets/Navbar.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Image from 'react-bootstrap/Image'
+import labLogo from '../assets/image/logo.png';
+import { Link } from "react-router-dom";
+
+function NavbarWeb() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <div className='brand'>
+            <Image src={labLogo}/>
+            <Navbar.Brand href='/'>Lab SCR <br/>(Sistem Cerdas & Robotika)</Navbar.Brand>
+        </div>
+        
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavDropdown title="Profile" id="basic-nav-dropdown">
+                <Link className='dropdownItem' to={"/"} >Visi Misi</Link>
+                <Link className='dropdownItem' to={"/pengurus"} >Pengurus</Link>
+            </NavDropdown>
+            <Link to={"/kegiatan"} >Kegiatan</Link>
+            <Link to={"/Berita"} >Berita</Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarWeb;

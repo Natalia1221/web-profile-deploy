e6263ffbd7b4b79ae6253c37b7a0577e66ddfd36
Natalia Matondang <nataliamtd.21@gmail.com>
2023-06-06 20:42:16 +0700

    menambahkan halaman pengurus

--- src/routes/Pengurus.jsx
+++ src/routes/Pengurus.jsx
@@ -1,14 +1,28 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
-function Pengurus() {
+const Pengurus = () => {
+  const [datas, setDatas] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://api.jsonbin.io/v3/b/642edc16c0e7653a059f0111", {
+      headers: {
+        "X-ACCESS-KEY": "$2b$10$yySJNemZxy5owr6fRQU62Ovqd/PLHW7.kg3KToeIMK5tCRqX398X."
+      }
+    })
+    .then((response) => response.json())
+    .then((json) => setDatas(json))
+    .then(()=>setLoading(false))
+  }, []);
   return (
-    <div>
-      <h1>Pengurus</h1>
-      <Link to="/">Go to Home page</Link>
-    </div>
+    <>
+      <div className="pengurus">
+      <img className="gambar_pengurus" src={loading?"":`${datas.record.pengurus}`}/>
+      </div>
+    </>
   );
-}
+};
 
 export default Pengurus;

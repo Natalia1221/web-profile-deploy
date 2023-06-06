9028383905d7e8d037fcfe5959c55be239a4a9c3
Natalia Matondang <nataliamtd.21@gmail.com>
2023-06-06 20:46:21 +0700

    menambahkan halaman kegiatan

---
+++ src/component/CardLayanan.jsx
@@ -0,0 +1,104 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+
+function GetData(){
+  const allData = fetch("https://api.jsonbin.io/v3/b/6463ac858e4aa6225e9e53fa", {
+      headers: {
+        "X-ACCESS-KEY": "$2b$10$yySJNemZxy5owr6fRQU62Ovqd/PLHW7.kg3KToeIMK5tCRqX398X."
+      }
+    })
+    .then((response) => response.json())
+  
+  return allData;
+}
+
+function CardPendidikan() {
+  
+  const [datas, setDatas] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    GetData()
+    .then((response) => setDatas(response))
+    .then(() =>setLoading(false))
+  }, []);
+  
+  return (
+    <Row xs={1} md={2} className="g-4">
+      {loading?( <p >Loading...</p>):(datas.record.BidangPendidikandanPengajaran.map((card)=>
+        {return <Col key={card.id}>
+                  <Card>
+                    <Card.Body>
+                      <Card.Title>{card.name}</Card.Title>
+                      <Card.Text className="description-kegiatan">{card.desc}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+        }))}
+    </Row>
+  );
+}
+
+function CardPenelitian() {
+  
+  const [datas, setDatas] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    GetData()
+    .then((response) => setDatas(response))
+    .then(() =>setLoading(false))
+  }, []);
+  return (
+    <Row xs={1} md={2} className="g-4">
+      {loading?( <p >Loading...</p>):(datas.record.BidangPenelitian.map((card)=>
+        {
+          
+          return <Col key={card.id}>
+                  <Card>
+                    <Card.Body>
+                      <Card.Title>{card.name}</Card.Title>
+                      { typeof card.desc === "string"?<Card.Text>{card.desc}</Card.Text> :(<ul>{card.desc.map((list, i)=>{
+                        return <li key={i}><Card.Text className="description-kegiatan">{list}</Card.Text></li>
+                      })}</ul>)
+                      }
+                    </Card.Body>
+                  </Card>
+                </Col>
+        }))}
+    </Row>
+  )
+}
+
+function CardAbdimas() {
+  
+  const [datas, setDatas] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    GetData()
+    .then((response) => setDatas(response))
+    .then(() =>setLoading(false))
+  }, []);
+  
+  return (
+    <Row xs={1} md={2} className="g-4">
+      {loading?( <p >Loading...</p>):(datas.record.BidangPengabdiankepadaMasyarakat.map((card)=>
+        {return <Col key={card.id}>
+                  <Card>
+                    <Card.Body>
+                      <Card.Title>{card.name}</Card.Title>
+                      <Card.Text className="description-kegiatan">{card.desc}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+        }))}
+    </Row>
+  );
+}
+
+export {CardAbdimas, CardPendidikan, CardPenelitian};
--- src/routes/Kegiatan.jsx
+++ src/routes/Kegiatan.jsx
@@ -1,14 +1,51 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import React from "react";
+import PenelitianPicture from '../assets/image/penelitian.svg';
+import PendidikanPicture from '../assets/image/pendidikan.svg';
+import AbdimasPicture from '../assets/image/Abdimas.svg';
+import {CardAbdimas, CardPendidikan, CardPenelitian} from '../component/CardLayanan'
+import $ from 'jquery';
 
-function Kegiatan() {
+const Kegiatan = () => {
+
   return (
-    <div>
-      <h1>Kegiatan</h1>
-      <Link to="/">Go to Home page</Link>
+    <div className="kegiatan">
+      <div className="box_kegiatan" id="pendidikan">
+        <img className="gambar_kegiatan" src={PendidikanPicture}/>
+        <div className="judul_kegiatan">
+          <h2 id="white_title">Bidang Pendidikan dan Pengajaran</h2>
+          <button className="toggle" onClick={()=>$('#target2').toggle('slow')}>&#x25BC;</button>
+        </div>
+        <div id="target2">
+        <CardPendidikan ></CardPendidikan>
+        </div>
+      </div>
+
+      <div className="box_kegiatan" id="penelitian">
+        <img className="gambar_kegiatan" src={PenelitianPicture}/>
+        <div className="judul_kegiatan">
+          <h2>Bidang Penelitian</h2>
+          <button className="toggle" onClick={()=>$('#target').toggle('slow')}>&#x25BC;</button>
+        </div>
+        <div id="target">
+        <CardPenelitian></CardPenelitian>
+        </div>
+      </div>
+
+      <div className="box_kegiatan" id="admas">
+        <img className="gambar_kegiatan" src={AbdimasPicture}/>
+        <div className="judul_kegiatan">
+          <h2 id="white_title">Bidang Pengabdian kepada Masyarakat (Abmas)</h2>
+          <button className="toggle" onClick={()=>$('#target3').toggle('slow')}>&#x25BC;</button>
+        </div>
+        <div id="target3">
+        <CardAbdimas ></CardAbdimas>
+        </div>
+      </div>
     </div>
+  
+    
   );
-}
+
+};
 
 export default Kegiatan;
